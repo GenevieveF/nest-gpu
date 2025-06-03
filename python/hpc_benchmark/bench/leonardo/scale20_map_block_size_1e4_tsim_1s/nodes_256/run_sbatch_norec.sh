587c5437e60c9f8e35fe04a4208e16ec023b35e2
Bruno Golosio <golosio@unica.it>
2025-06-03 23:15:38 +0200

    Added further directories for HPC benchmark assesment on Leonardo

---
+++ python/hpc_benchmark/bench/leonardo/scale20_map_block_size_1e4_tsim_1s/nodes_256/run_sbatch_norec.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+#SBATCH --account=IscrB_ngpu-opt
+#SBATCH --nodes=256
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=8
+#SBATCH --time=00:30:00
+#SBATCH --partition=boost_usr_prod
+#SBATCH --qos=boost_qos_bprod
+#SBATCH --gres=gpu:4
+#SBATCH --gpus-per-task=1
+#SBATCH --exclusive
+#SBATCH --output=/leonardo_scratch/large/userexternal/bgolosio/test_ngpu_hpcb_wg_out.%j
+#SBATCH --error=/leonardo_scratch/large/userexternal/bgolosio/test_ngpu_hpcb_wg_err.%j
+# *** start of job script ***
+# Note: The current working directory at this point is
+# the directory where sbatch was executed.
+
+###export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+srun python3 hpc_benchmark.py --scale 20.0 --simtime 1000.0 --record_spikes 0
---
+++ python/hpc_benchmark/bench/leonardo/scale20_map_block_size_1e4_tsim_1s/nodes_32_fake/run_sbatch_fake.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+#SBATCH --account=IscrB_ngpu-opt
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=32
+#SBATCH --time=00:30:00
+#SBATCH --partition=boost_usr_prod
+#SBATCH --qos=boost_qos_dbg
+####SBATCH --gres=gpu:4
+#SBATCH --gpus-per-task=1
+#SBATCH --exclusive
+#SBATCH --output=/leonardo_scratch/large/userexternal/bgolosio/test_ngpu_hpcb_wg_out.%j
+#SBATCH --error=/leonardo_scratch/large/userexternal/bgolosio/test_ngpu_hpcb_wg_err.%j
+# *** start of job script ***
+# Note: The current working directory at this point is
+# the directory where sbatch was executed.
+
+###export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+srun python3 hpc_benchmark.py --scale 20.0 --simtime 1000.0 --fake_mpi_proc_num 128 --fake_mpi_proc_id 0
