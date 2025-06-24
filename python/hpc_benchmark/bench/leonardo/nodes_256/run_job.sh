9e91dc3cdae5385c1ab7d08b3c5021083cd0ed99
Bruno Golosio <golosio@unica.it>
2025-06-24 15:10:24 +0200

    Reorganizing HPC benchmarking folders and scripts for Leonardo

--- python/hpc_benchmark/bench/leonardo/nodes_256/run_job.sh
+++ python/hpc_benchmark/bench/leonardo/nodes_256/run_job.sh
@@ -4,8 +4,7 @@ if [ "$#" -ne 2 ]; then
     echo "Usage: $0 opt run"
     echo "opt [0-3]: optimization gpu-memory-vs-speed, 3 for no spike recording"
     echo "run [0-9]: run number (changes output folder and random seed)"
-    exit 2
+else
+    cat ../run_sbatch.templ | sed 's/__nodes__/$nodes/' > run_sbatch.sh
+    sbatch run_sbatch.sh
 fi
-
-cat ../run_sbatch.templ | sed 's/__nodes__/$nodes/' > run_sbatch.sh
-sbatch run_sbatch.sh
